f24c1ba3c15a39810af78823749abe59ad86f425
Nikola Maksimovic <nikola.maksimovic.jobs@gmail.com>
2025-01-15 17:04:52 +0100

    Created observables by using of and from Rxjs operators.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { from, of, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,36 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'ng-rxjs-signals';
+
+  sub!: Subscription;
+  subArray!: Subscription;
+  subFrom!: Subscription;
+  subString!: Subscription;
+
+  ngOnInit(): void {
+    this.sub = of(2, 4, 6, 8, 10).subscribe(item => console.log("Value from of:", item));
+    
+    this.subArray = of([2, 4, 6, 8, 10]).subscribe(item => console.log("Value from of array:", item));
+
+    this.subFrom = from([20, 40, 60, 80, 100]).subscribe({
+      next: (item) => console.log("Value from from:", item),
+      error: (error) => console.log("Error from from:", error),
+      complete: () => console.log("Complete from from")
+    });
+    
+    this.subString = of("Apple1", "Apple2", "Apple3", "Apple4", "Apple5").subscribe({
+      next: (item) => console.log("Apple value from of:", item),
+      error: (error) => console.log("Error from apple of:", error),
+      complete: () => console.log("Complete from apple of")
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+    this.subArray.unsubscribe();
+    this.subFrom.unsubscribe();
+    this.subString.unsubscribe();
+  }
 }
