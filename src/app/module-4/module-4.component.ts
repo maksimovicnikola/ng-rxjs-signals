92d2a029a43ea15120a19015ed33bfe94eebe8a1
Nikola Maksimovic <nikola.maksimovic.jobs@gmail.com>
2025-01-17 14:32:28 +0100

    Split module 3 and module 4; cleaned up app component.

---
+++ src/app/module-4/module-4.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-module-4',
+  imports: [],
+  templateUrl: './module-4.component.html',
+  styleUrl: './module-4.component.scss'
+})
+export class Module4Component {
+
+}
