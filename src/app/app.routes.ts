ff480434ff6fabc92eb40512c35025e5b49786ef
Nikola Maksimovic <nikola.maksimovic.jobs@gmail.com>
2025-01-20 14:10:04 +0100

    Removed module 3 and module 4 code; initialized module-5 starter code.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './utilities/page-not-found.component';
 
 export const routes: Routes = [
-    {
-        path: 'module-3',
-        loadComponent: () => import('./module-3/module-3.component').then(m => m.Module3Component)
-    },
-    {
-        path: 'module-4', 
-        loadComponent: () => import('./module-4/module-4.component').then(m => m.Module4Component)
-    }
-];
+  { path: 'welcome', component: HomeComponent },
+  {
+    path: 'products',
+    loadComponent: () => import('./products/product-list/product-list.component').then(c => c.ProductListComponent)
+  },
+  {
+    path: 'cart',
+    loadComponent: () => import('./cart/cart-shell/cart-shell.component').then(c => c.CartShellComponent)
+  },
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }];
