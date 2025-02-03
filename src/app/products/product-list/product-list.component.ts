a6c91dc8b6d3010ce94bdb008af0f147b9a3ffb7
Nikola Maksimovic <nikola.maksimovic.jobs@gmail.com>
2025-02-03 15:25:42 +0100

    Transformed product-list to use a declarative approach instead the procedural one; Cached products via shareReplay.

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,45 +1,31 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 
-import { NgIf, NgFor, NgClass } from '@angular/common';
-import { Product } from '../product';
+import { NgIf, NgFor, NgClass, AsyncPipe } from '@angular/common';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 import { ProductService } from '../product.service';
-import { catchError, EMPTY, Subscription, tap } from 'rxjs';
+import { catchError, EMPTY, tap } from 'rxjs';
 
 @Component({
   selector: 'pm-product-list',
   templateUrl: './product-list.component.html',
   standalone: true,
-  imports: [NgIf, NgFor, NgClass, ProductDetailComponent],
+  imports: [NgIf, NgFor, NgClass, AsyncPipe, ProductDetailComponent],
 })
-export class ProductListComponent implements OnInit, OnDestroy {
+export class ProductListComponent {
   pageTitle = 'Products';
   errorMessage = '';
 
   private productService = inject(ProductService);
 
-  sub!: Subscription;
+  readonly products$ = this.productService.products$.pipe(
+    catchError((err) => {
+      this.errorMessage = err;
 
-  products: Product[] = [];
+      return EMPTY;
+    })
+  );
+
   selectedProductId: number = 0;
-
-  ngOnInit(): void {
-    this.sub = this.productService
-      .getProducts()
-      .pipe(
-        tap(() => console.log('In component pipeline')),
-        catchError(err => {
-          this.errorMessage = err
-
-          return EMPTY;
-        })
-      )
-      .subscribe((products) => (this.products = products));
-  }
-
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
 
   onSelected(productId: number): void {
     this.selectedProductId = productId;
